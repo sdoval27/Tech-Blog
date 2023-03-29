dd166e2ea83a4601a4b736262724f161f898a9a6
unknown <sashardoval@gmail.com>
2023-03-29 12:50:31 -0700

    create comment model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Project, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -53,15 +53,23 @@ router.get('/project/:id', async (req, res) => {
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   attributes: { exclude: ['password'] },
+    //   include: [{ model: Project }],
+    //});
 
-    const user = userData.get({ plain: true });
+    //const user = userData.get({ plain: true });
+    const projectData = await Project.findAll({
+      where: {
+        user_id: req.session.user_id,
 
+      }
+    })
+    const projects = projectData.map((project)=>project.get({plain:true}));
+    console.log(projects);
+    
     res.render('profile', {
-      ...user,
+      projects,
       logged_in: true
     });
   } catch (err) {
--- models/index.js
+++ models/index.js
@@ -1,13 +1,22 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
 Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Project.hasMany(Comment, {
+  foreignKey: 'project_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project, Comment };
